3da7ba9702b8b3f624ef90a450463194ad924b6c
Ananya kumari <raiananya1910@gmail.com>
2026-07-23 12:20:59 +0530

    second commit

--- LAB1/fun.js
+++ LAB1/fun.js
@@ -1,1 +1,11 @@
- 
+ //write a program to take any digit (0-9)and 
+ //return its in word 
+ //0-one 
+ //5-five 
+ const toWords = (digit )=>{
+    const words =["zero","one","two","three","four","five","six","seven","eight","nine"];
+    return words[digit];
+ }
+ console.log(toWords(5));
+ console.log(toWords(2));
+ console.log(toWords(7));
