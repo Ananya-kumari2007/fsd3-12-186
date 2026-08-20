ea4281a5dafc8147fc918857dd66ae85831b1e7e
Ananya kumari <raiananya1910@gmail.com>
2026-08-20 11:18:04 +0530

    crud

--- lab2/file1.js
+++ lab2/file1.js
@@ -1,5 +1,7 @@
-import { appendFile } from"fs/promises";
+import { writeFile , appendFile ,readFile } from"fs/promises";
 
-//await writeFile("hello.txt","js is not easy");
+await writeFile("hello.txt","js is not easy");
 await appendFile("hello.txt","\nFs is much easy than others");
-
+await appendFile("hello.txt","\nAnanya 🤗");
+const content = await readFile("hello.txt","utf-8");
+console.log(content);
